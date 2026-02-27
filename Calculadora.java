b086186cb053cfd34f2e74588a342ede2e3839bb
Zanty1651 <santiloaiza2000@gmail.com>
2026-02-26 20:24:24 -0500

    Agregada funcion RESTA

--- Calculadora.java
+++ Calculadora.java
@@ -6,5 +6,12 @@ public class Calculadora {
     public static double sumar(double a, double b) {
         return a + b;
     }
+
+
+
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
 }
 
